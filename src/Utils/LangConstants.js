d0609ef67c50beba1bd9b20599e1e470c2e2e61c
Tushar Pal <Tusharpal353@gmail.com>
2024-08-21 18:17:43 +0530

    Made Language option (for fun)

---
+++ src/Utils/LangConstants.js
@@ -0,0 +1,17 @@
+const lang= {
+    en:{
+        search:"search",
+        gptSearchPlaceHolder:"what would you like to watch ?",
+
+    },
+    hindi:{
+        search:"खोज",
+        gptSearchPlaceHolder:"आप क्या देखना चाहेंगे?",
+
+    },
+    German:{
+        search:"suchen",
+        gptSearchPlaceHolder:"Was möchtest du sehen?    ",
+    }
+}
+export default lang;
--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,14 +1,21 @@
-export const LOGO= "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-export const BANNER= "https://assets.nflxext.com/ffe/siteui/vlv3/b2c3e95b-b7b5-4bb7-a883-f4bfc7472fb7/19fc1a4c-82db-4481-ad08-3a1dffbb8c39/IN-en-20240805-POP_SIGNUP_TWO_WEEKS-perspective_WEB_24a485f6-1820-42be-9b60-1b066f1eb869_large.jpg"
-export const API_OPTIONS= {
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const BANNER =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/b2c3e95b-b7b5-4bb7-a883-f4bfc7472fb7/19fc1a4c-82db-4481-ad08-3a1dffbb8c39/IN-en-20240805-POP_SIGNUP_TWO_WEEKS-perspective_WEB_24a485f6-1820-42be-9b60-1b066f1eb869_large.jpg";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjViYzRkODUwYTVhZDA1MTJhNWQyYjdmYjA4Mjg4YiIsIm5iZiI6MTcyMzk5NDExNS41NTk2MzgsInN1YiI6IjY2YzIwZWNmYjZlNGQ3ODdjMWQzNDk2MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t5dFchSSlqKK6Qy7ZLjS9iAzSUsjBZTOlifUSNG5ZSQ",
+  },
+};
 
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjViYzRkODUwYTVhZDA1MTJhNWQyYjdmYjA4Mjg4YiIsIm5iZiI6MTcyMzk5NDExNS41NTk2MzgsInN1YiI6IjY2YzIwZWNmYjZlNGQ3ODdjMWQzNDk2MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t5dFchSSlqKK6Qy7ZLjS9iAzSUsjBZTOlifUSNG5ZSQ'
-        }
-      };
+//AIzaSyBGSdUGrT8zzoj8vpCfwA-4dp418noAVCw
+//fefeee46
 
-
-      //AIzaSyBGSdUGrT8zzoj8vpCfwA-4dp418noAVCw
-      //fefeee46
+export const SUPPORTED_LANGUAGE = [
+  { identifier: "eng", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "german", name: "German" },
+];
