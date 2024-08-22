59246e6b6a41f7184a9601bf5581fb5317aae2b8
Tushar Pal <Tusharpal353@gmail.com>
2024-08-22 20:53:44 +0530

    Changed the API from GPT to the GEMINI

--- src/Utils/openAI.js
+++ src/Utils/openAI.js
@@ -1,15 +1,3 @@
 import OpenAI from 'openai';
 import { OPENAI_KEY } from './constants';
-/* const client = new OpenAI({
-  apiKey: process.env[OPENAI_KEY], // This is the default and can be omitted
-  dangerouslyAllowBrowser: true
-}); */
 
-/* async function main() {
-  const chatCompletion = await client.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test' }],
-    model: 'gpt-3.5-turbo',
-  });
-}
- */
-// main();
